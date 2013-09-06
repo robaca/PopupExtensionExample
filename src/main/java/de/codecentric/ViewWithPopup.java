c4587eb2f38b6d6ab79b6e21965f19152da28c95
Carsten Mjartan <carsten.mjartan@codecentric.de>
2013-09-06 08:38:41 +0200

    ComboBox issue

--- src/main/java/de/codecentric/ViewWithPopup.java
+++ src/main/java/de/codecentric/ViewWithPopup.java
@@ -1,39 +1,53 @@
 package de.codecentric;
 
 import com.github.wolfie.popupextension.PopupExtension;
+import com.github.wolfie.popupextension.PopupExtension.PopupExtensionManualBundle;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 public class ViewWithPopup extends VerticalLayout implements View {
 
-	private Button button;
+    private Button button;
 
-	public ViewWithPopup(String title) {
-		addComponent(new Label(title));
-		
-		button = new Button("Open Popup");
-		final PopupExtension popupExtension = PopupExtension.extend(button);
-		popupExtension.setOffset(100, 50);
-		popupExtension.setContent(new Label("POPUP CONTENT"));
+    public ViewWithPopup(String title) {
+        addComponent(new Label(title));
 
-		button.addClickListener(new ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				popupExtension.toggle();
-			}
-		});
-		addComponent(button);
-	}
+        button = new Button("Open Popup");
+        PopupExtensionManualBundle bundle = PopupExtension.extendWithManualBundle(button);
 
-	@Override
-	public void enter(ViewChangeEvent event) {
+        addComponent(bundle.getDataTransferComponent());
 
-	}
+        final PopupExtension popupExtension = bundle.getPopupExtension();
+        popupExtension.setOffset(100, 50);
+        popupExtension.setDirection(Alignment.BOTTOM_RIGHT);
+
+        FormLayout popupLayout = new FormLayout();
+        popupLayout.setWidth("500px");
+        popupLayout.addComponent(new Label("Problematic Combobox (press F5)"));
+        popupLayout.addComponent(new ComboBox("POPUP CONTENT"));
+        popupExtension.setContent(popupLayout);
+
+        button.addClickListener(new ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                popupExtension.toggle();
+            }
+        });
+        addComponent(button);
+    }
+
+    @Override
+    public void enter(ViewChangeEvent event) {
+
+    }
 
 }
